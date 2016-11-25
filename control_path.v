f61f2359782c6621eff2aa6584d896588fcf2c2e
Luis Santos <lasantos2@outlook.com>
2016-11-25 15:18:45 -0700

    Added some notes from the meeting, and added on the verilog code that I worked on

---
+++ control_path.v
@@ -0,0 +1,16 @@
+module control(
+  input wire [5:0]  Opcode,
+  output reg RegDst,
+  output reg Branch,
+  output reg MemtoRead,
+  output reg MemtoReg,
+  output reg [1:0] ALUOp,
+  output reg MemtoWrite,
+  output reg ALUSrc,
+  output reg RegWrite);
+
+  
+
+
+
+endmodule
