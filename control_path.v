7bd230e27f004c51974f86734f5910f85deff41a
Luis Santos <lasantos2@outlook.com>
2016-11-25 17:04:53 -0700

    Untested but finished Control Path module

--- control_path.v
+++ control_path.v
@@ -9,8 +9,36 @@ module control(
   output reg ALUSrc,
   output reg RegWrite);
 
-  
+  always@(Opcode) begin
+      case (Opcode)
+        0 : begin RegDst = 1;
+            Branch = 0;
+            MemtoRead = 0;
+            MemtoReg = 0;
+            ALUOp = 2;
+            MemtoWrite = 0;
+            ALUSrc = 0;
+            RegWrite = 1;
+            end
+        35:begin
+            RegDst = 0;
+            ALUSrc = 1;
+            MemtoReg = 1;
+            RegWrite = 1;
+            MemtoRead = 1;
+            MemtoWrite = 0;
+            Branch = 0;
+            ALUOp = 0;
+            end
+        43:begin
+            ALUSrc = 1;
+            RegWrite = 0;
+            MemtoRead = 0;
+            MemtoWrite = 1;
+            Branch = 0;
+            ALUOp = 0;
+           end
 
-
-
+        endcase
+      end
 endmodule
