be8a4396f07595ea0526684beba9b1daa26439b3
Luis Santos <lasantos2@outlook.com>
2016-11-28 16:24:23 -0700

    Added Memory into the main code, and added Mcgarries spec module

--- Gforce_cpu.v
+++ Gforce_cpu.v
@@ -63,7 +63,6 @@ module ALUControl (ALUOp , Function , Output );
   endmodule
 
 /* Data Path Muxes - by Kevin Valdez
-
    Multiplexers that are part of the cpu datapah that are controlled by
    the control signals RegDst, ALUSrc, MemtoReg.
 */
@@ -140,7 +139,6 @@ module instructmem(
 
     endmodule
 
-
 /* Control - by Luis Santos
 
    Takes the Opcode sent to it and sends
@@ -193,3 +191,48 @@ module control(
             endcase
           end
     endmodule
+
+/* Data Memory - Eberado Sanchez*/
+
+module Memory (CS, WE, ClK, ADDR, Mem_Bus);
+
+    	input CS, WE, ClK;
+    	input [31:0] ADDR;
+    	inout[31:0] Mem_Bus;
+
+    	reg [31:0] data_out;
+    	reg [31:0] RAM [0:127];
+
+    	integer i;
+    	reg [6:0] counter;
+
+    	initial begin
+    		for(i=0; i<128; i=i+1)
+    			begin
+    				RAM[i] = 32'd0;
+    			end
+
+    	end
+
+    	assign Mem_Bus = ((CS == 1'b0) || (WE == 1'b1)) ? 32'bZ : data_out;
+
+    	always @(negedge ClK)
+    	begin
+    		if ((CS == 1'b1) && (WE == 1'b1))
+    			RAM[ADDR] <= Mem_Bus[31:0];
+    		data_out <= RAM[ADDR];
+    	end
+
+    endmodule
+/*Main mipscpu*/
+module mipscpu(
+    input wire reset,
+    input wire clock,
+    input wire [31:0] instrword,
+    input wire newinstr);
+
+
+
+
+
+endmodule
